6aea2d138b757fe9b59ba3ae595c58b0e032dbe6
sharmashekhar0 <beingperfect76@gmail.com>
2023-12-09 21:07:30 +0530

    add structural changes

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -7,7 +7,7 @@ import Technologies from "./Technologies";
 
 function Hero() {
 	return (
-		<div className="min-h-screen bg-[#001840] text-white px-12 flex flex-col gap-12 pb-8">
+		<div className="min-h-screen bg-[#001840] text-white px-12 flex flex-col gap-12 pb-4git">
 			<Bio />
 			<Skills />
 			<Technologies />
